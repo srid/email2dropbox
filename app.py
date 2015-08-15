bccbd4967a00e201dfbd93f7b0523127156c4cad
Sridhar Ratnakumar <srid@srid.ca>
2015-08-15 16:56:38 -0700

    Initial work on dropbox, untested

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+from contextlib import contextmanager
 import logging
 log = logging.getLogger(__name__)
 
@@ -6,13 +7,24 @@ from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
 from pyramid.view import view_config
+import pyramid.httpexceptions as exc
 
+from dropbox.client import DropboxOAuth2Flow, DropboxClient
 from postmark_inbound import PostmarkInbound
 
 
 @view_config(name='', request_method='GET')
 def index(request):
-    return Response('Nothing here, move along')
+    flow = make_dropbox_auth_flow(request.session)
+    authorize_url = flow.start()
+    return exc.HTTPFound(location=authorize_url)
+
+@view_config(name='dropboxauth', request_method='POST')
+def dropboxauth(request):
+    with handle_dropbox_redirect():
+        flow = make_dropbox_auth_flow(request.session)
+        access_token, user_id, url_state = flow.finish(request.GET)
+    return HTTPFound(location="/home")
 
 @view_config(name='incoming', request_method='POST')
 def incoming(request):
@@ -20,7 +32,9 @@ def incoming(request):
     log.error("Received email: %s", inbound.subject())
     return Response("Ok")
 
-if __name__ == '__main__':
+
+def configure_webapp():
+
     config = Configurator()
     config.scan()
     app = config.make_wsgi_app()
@@ -30,5 +44,41 @@ if __name__ == '__main__':
     log.addHandler(h)
 
     port = int(os.environ.get("PORT", 8080))
-    server = make_server('0.0.0.0', port, app)
+    return make_server('0.0.0.0', port, app)
+
+def configure_dropbox():
+    app_key = os.getenv("DROPBOX_APP_KEY")
+    app_secret = os.getenv("DROPBOX_APP_SECRET")
+    if app_key is None or app_secret is None:
+        raise ValueError("DROPBOX env vars not set")
+    return app_key, app_secret
+
+def make_dropbox_auth_flow(session):
+    app_key, app_secret = configure_dropbox()
+    flow = DropboxOAuth2Flow(app_key, app_secret,
+                             request.session, "dropbox-auth-csrf-token")
+    return flow
+
+@contextmanager
+def handle_dropbox_redirect():
+    try:
+        yield
+    except DropboxOAuth2Flow.BadRequestException, e:
+        raise exc.HTTPBadRequest()
+    except DropboxOAuth2Flow.BadStateException, e:
+        log.info('Bad state; trying again.')
+        # Start the auth flow again.
+        pass # return HTTPFound(location="/home")
+    except DropboxOAuth2Flow.CsrfException, e:
+        raise exc.HTTPForbidden()
+    except DropboxOAuth2Flow.NotApprovedException, e:
+        log.error('Not approved?  Why not?')
+        pass # return HTTPFound(location="/home")
+    except DropboxOAuth2Flow.ProviderException, e:
+        log.error("Auth error: %s" % (e,))
+        raise exc.HTTPForbidden()
+
+if __name__ == '__main__':
+    configure_dropbox()
+    server = configure_webapp()
     server.serve_forever()
