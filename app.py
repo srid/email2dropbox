df381db8be7343fd97c6d6508fc26d9a19a3326e
Sridhar Ratnakumar <srid@srid.ca>
2015-08-15 14:40:58 -0700

    Add /incoming webhook placeholder

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import os
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return '\n'.join(map(str, os.environ.items()))
+
+@app.route('/incoming', methods=['POST'])
+def incoming():
+    return request.get_data()
