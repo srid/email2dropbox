fad286ac6216097dd293ef830cafc6256d65750f
Sridhar Ratnakumar <srid@srid.ca>
2015-08-15 16:22:26 -0700

    Use declarative config

--- app.py
+++ app.py
@@ -5,26 +5,27 @@ log = logging.getLogger(__name__)
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
+from pyramid.view import view_config
 
 from postmark_inbound import PostmarkInbound
 
 
+@view_config(name='', request_method='GET')
 def index(request):
-    return Response('Hello world')
+    return Response('Nothing here, move along')
 
+@view_config(name='incoming', request_method='POST')
 def incoming(request):
-    inbound = PostmarkInbound(json=request.body)
-    log.error("Received subject: %s", inbound.subject())
+    #inbound = PostmarkInbound(json=request.body)
+    #log.error("Received email: %s", inbound.subject())
+    log.error("Received %s", request.body)
     return Response("Ok")
-
 
 if __name__ == '__main__':
     config = Configurator()
-    config.add_route('index', '/')
-    config.add_view(index, route_name='index')
-    config.add_route('webhook', '/incoming')
-    config.add_view(incoming, route_name='webhook')
+    config.scan()
     app = config.make_wsgi_app()
+
     port = int(os.environ.get("PORT", 8080))
     server = make_server('0.0.0.0', port, app)
     server.serve_forever()
